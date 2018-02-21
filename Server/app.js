965c81d7f77c46f2fb31a6ab0474473b99044309
Harrison <mvharrison@labs.neit.edu>
2018-02-21 12:55:48 -0500

    2/21/18 Unity Socket IO Intro

---
+++ Server/app.js
@@ -0,0 +1,35 @@
+var io = require('socket.io')(process.envPort||3000);
+var shortid = require('shortid');
+
+console.log("Server Started");
+//console.log(shortid.generate());
+
+var players =[];
+
+io.on('connection', function(socket){
+	
+	var thisPlayerId = shortid.generate();
+	players.push(thisPlayerId);
+	
+	console.log('client connected spawning player id: ' + thisPlayerId);
+	
+	socket.broadcast.emit('spawn player', {id:thisPlayerId});
+	
+	players.forEach(function(playerId){
+		if(playerId == thisPlayerId) return;
+	
+		socket.emit('spawn player', {id:playerId});
+		console.log("Adding a new player " + playerId);
+	})
+	
+	socket.on('playerhere', function(data){
+		console.log("Player is logged in!");
+	});
+	
+	socket.on('disconnect', function(){
+		console.log("Player Disconnected");
+		players.splice(players.indexOf(thisPlayerId), 1);
+		socket.broadcast.emit('disconnected', {id:thisPlayerId});
+	});
+
+});
---
+++ SocketIOProject/SocketIOProject/Assets/Network.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Network : MonoBehaviour {
+	static SocketIOComponent socket;
+	public GameObject playerPrefab;
+
+    Dictionary<string, GameObject> players;
+
+	// Use this for initialization
+	void Start () {
+		socket = GetComponent<SocketIOComponent> ();
+		socket.On ("open", OnConnected);
+		socket.On ("spawn player", OnSpawned);
+        socket.On("disconnected", OnDisconnected);
+        players = new Dictionary<string, GameObject>();
+
+    }
+	
+	// Tells us we are connected
+	void OnConnected (SocketIOEvent e) {
+		Debug.Log ("We are Connected");
+		socket.Emit ("playerhere");
+	}
+
+	void OnSpawned(SocketIOEvent e){
+		Debug.Log ("Player Spawned!" + e.data);
+		var player = Instantiate (playerPrefab);
+        players.Add(e.data["id"].ToString(), player);
+        Debug.Log("count: " + players.Count);
+	}
+
+    void OnDisconnected(SocketIOEvent e)
+    {
+        Debug.Log("Player Disconnected!" + e.data);
+        Destroy(players[e.data["id"].ToString()]);
+        players.Remove(e.data["id"].ToString());
+        Debug.Log("count: " + players.Count);
+    }
+}
